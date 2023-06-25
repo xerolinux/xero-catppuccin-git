9dcf2600b6d6d71099eb3744e26b6007ed1106b6
TechXero <steve@techxero.com>
2023-06-25 10:10:11 +0300

    Update

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ echo
 echo "Installing Catppuccin Theme & Packages"
 echo "######################################"
 # Check if any of the specified packages are installed and install them if not present
-packages="lightly-git xero-kde-config xero-catppuccin-sddm asian-fonts lightlyshaders-git xero-catppuccin-wallpapers tela-circle-icon-theme-dracula-git python-pip gnome-themes-extra gtk-engine-murrine gtk-engines xero-fonts-git"
+packages="lightly-git xero-catppuccin-sddm asian-fonts lightlyshaders-git xero-catppuccin-wallpapers tela-circle-icon-theme-dracula-git python-pip gnome-themes-extra gtk-engine-murrine gtk-engines xero-fonts-git"
 echo
 echo "Installing required packages..."
 echo "###############################"
@@ -36,10 +36,24 @@ for package in $packages; do
 done
 sleep 2
 echo
+echo "Creating Backup & Applying new Rice, hold on..."
+echo "###############################################"
+cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S) && cp -Rf Configs/Home/. ~ && cp -Rf Configs/Home/.config/latte ~/.config/
+sudo cp -Rf Configs/System/. / && sudo cp -Rf Configs/Home/. /root/
+sleep 2
+echo
+echo "Applying Our Custom Grub Theme..."
+echo "#################################"
+chmod +x Grub.sh
+sudo ./Grub.sh
+sudo sed -i "s/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1920x1080/g" /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sleep 2
+echo
 # Clone GTK theme repository and install
 echo "Installing catppuccin GTK4 theme & Applying LibAdwaita Patch"
 echo "############################################################"
-git clone --recurse-submodules https://github.com/catppuccin/gtk.git && cd gtk/ && pip3 install -r requirements.txt
+git clone --recurse-submodules https://github.com/catppuccin/gtk.git && cd gtk/
 python install.py mocha -l -a mauve --tweaks normal -d ~/.themes && cd .. && rm -Rf gtk/
 sleep 2
 echo
@@ -55,12 +69,6 @@ echo "##############################"
 sudo sed -i "s/Current=.*/Current=catppuccin/g" /etc/sddm.conf.d/kde_settings.conf 2>/dev/null
 sleep 2
 echo
-echo "Creating Backup & Applying new Rice, hold on..."
-echo "###############################################"
-cp -Rf ~/.config ~/.config-backup-$(date +%Y.%m.%d-%H.%M.%S) && cp -Rf Configs/Home/. ~ && cp -Rf Configs/Home/.config/latte ~/.config/
-sudo cp -Rf Configs/System/. / && sudo cp -Rf Configs/Home/. /root/
-sleep 2
-echo
 echo "Applying Flatpak GTK Overrides"
 echo "##############################"
 sudo flatpak override --filesystem=$HOME/.themes
@@ -73,14 +81,7 @@ echo "###################################"
 sleep 1.5
 stylepak install-system Catppuccin-Mocha-Standard-Mauve-Dark && stylepak install-user Catppuccin-Mocha-Standard-Mauve-Dark
 sleep 2
-echo
-echo "Applying Our Custom Grub Theme..."
-echo "#################################"
-chmod +x Grub.sh
-sudo ./Grub.sh
-sudo sed -i "s/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1920x1080/g" /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-sleep 2
+rm -rf xero-catppuccin-git/
 echo
 echo "#############################################"
 echo "  All Done! Reboot system To activate rice.  "
